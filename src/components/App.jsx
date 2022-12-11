41669ddaf537fa0812a2f5641e0065c338ab3a57
Yuliia Dikun <dikun.yulia@gmail.com>
2022-12-11 19:05:23 +0200

    add fourth task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import user from '../data/user';
 import data from '../data/data.json';
 import friends from '../data/friends'
+import history from '../data/transactions';
 import { User } from "./Profile";
 import { Statistics } from "./Statistics";
 import { FriendsList } from './Friends';
-console.log(friends);
+import { TransactionHistory } from './TransactionHistory';
+console.log(history);
 export const App = () => {
   return (
     <div
@@ -28,7 +30,8 @@ export const App = () => {
         
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
-      <FriendsList friends={ friends} />
+      <FriendsList friends={friends} />
+      <TransactionHistory history={history} />
       </div>      
   );
 };
---
+++ src/components/TransactionHistory.jsx
@@ -0,0 +1,22 @@
+import css from './TransactionHistory.module.css';
+export const TransactionHistory = ({ history }) => (
+    <table className={css.transactionhistory}>
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {history.map(({ id, amount, currency, type }) => (
+        <tr key={id}>
+          <td>{type}</td>
+          <td>{amount}</td>
+          <td>{currency}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
