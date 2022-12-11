7ed74dc238ab4fe9a334a5b74a5741876fce96d6
Yuliia Dikun <dikun.yulia@gmail.com>
2022-12-11 16:33:15 +0200

    add first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import user from '../data/user';
+import { User } from "./Profile";
 export const App = () => {
   return (
     <div
@@ -9,8 +11,13 @@ export const App = () => {
         fontSize: 40,
         color: '#010101'
       }}
-    >
-      React homework template
-    </div>
+    >      
+      <User
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}/>
+      </div>
   );
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import './Profile.css';
+export const User = ({avatar,username, tag, location}) => (
+<div className="profile">
+    <div className="description">
+                <img
+                    src={avatar}
+        alt="User avatar"
+        className="avatar"
+        />
+                <p className="name">{username}</p>
+                <p className="tag">{tag}</p>
+                <p className="location">{location}</p>
+    </div>
+
+  <ul className="stats">
+    <li>
+      <span className="label">Followers</span>
+      <span className="quantity">1000</span>
+    </li>
+    <li>
+      <span className="label">Views</span>
+      <span className="quantity">2000</span>
+    </li>
+    <li>
+      <span className="label">Likes</span>
+      <span className="quantity">3000</span>
+    </li>
+  </ul>
+</div>
+);
